dbbe70a8fa7dfbae75cfdbbcf873118687b3be4a
Dipesh Dhote <dpsh.dhote@gmail.com>
2024-04-23 00:19:22 +0530

    add

--- app.py
+++ app.py
@@ -7,17 +7,22 @@ def recommend(product):
     name_index = products[products['name'].str.contains(product)].index[0]
     distances = sorted(list(enumerate(similarity[name_index])),reverse = True ,key=lambda x:x[1])
 
-    recommended_product =[]
+    r_product =[]
     recommended = []
-    recommended_img = []
-    for i in distances[1:6]:
-        recommended_product.append(products.iloc[i[0]]['name'])
+    r_img = []
+    r_variant = []
+    r_mrp = []
+
+    for i in distances[1:4]:
+        r_product.append(products.iloc[i[0]]['name'])
         recommended.append(products.iloc[i[0]]['image_url'])
+        r_variant.append(products.iloc[i[0]]['variant'])
+        r_mrp.append(products.iloc[i[0]]['mrp'])
 
     for i in recommended:
-        recommended_img.append(i.replace(" ",""))
+        r_img.append(i.replace(" ",""))
         
-    return recommended_product,recommended_img
+    return r_product,r_img,r_variant,r_mrp
 
 st.header('Grocery Recommendation System')
 
@@ -27,27 +32,26 @@ similarity = pickle.load(open('artifacts/similarity.pkl','rb'))
 
 
 product_list = products['name'].values
-selected_product = st.selectbox('select the product',
+selected_product = st.selectbox('select product ',
                       product_list)
 
 
 if st.button('Recommend'):
-    recommended_product,recommended_img = recommend(selected_product)
-    col1, col2, col3, col4, col5 = st.columns(5)
+    r_product,r_img,r_variant,r_mrp = recommend(selected_product)
+    col1, col2, col3 = st.columns(3)
     with col1:
-        st.text(recommended_product[0])
-        st.image(recommended_img[0])
+        st.text(r_product[0])
+        st.image(r_img[0])
+        st.text('Quantity :{}'.format(r_variant[0]))
+        st.text('MRP :{}'.format(r_mrp[0]))
     with col2:
-        st.text(recommended_product[1])
-        st.image(recommended_img[2])
-
+        st.text(r_product[1])
+        st.image(r_img[1])
+        st.text('Quantity :{}'.format(r_variant[1]))
+        st.text('MRP :{}'.format(r_mrp[1]))
+        
     with col3:
-        st.text(recommended_product[2])
-        st.image(recommended_img[2])
-    with col4:
-        st.text(recommended_product[3])
-        st.image(recommended_img[3])
-    with col5:
-        st.text(recommended_product[4])
-        st.image(recommended_img[4])
-
+        st.text(r_product[2])
+        st.image(r_img[2])
+        st.text('Quantity :{}'.format(r_variant[2]))
+        st.text('MRP :{}'.format(r_mrp[2]))
